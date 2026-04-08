abf629bc80bdc52477ba0d4df6a86de87a530cb5
sverma096 <tiger543939@gmail.com>
2026-04-08 12:35:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/saas", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tools", toolRoutes);
@@ -22,4 +21,8 @@ app.get("/", (req, res) => {
   res.send("Backend Running 🚀");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
