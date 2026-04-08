128d6ade8be2dc1a567c417fc0598e8ccb8a5efc
sverma096 <tiger543939@gmail.com>
2026-04-08 12:58:33 +0530

    Update pdf.js

--- utils/pdf.js
+++ utils/pdf.js
@@ -1,16 +1,10 @@
-const { PDFDocument } = require("pdf-lib");
-
-exports.mergePDF = async (req, res) => {
-  const mergedPdf = await PDFDocument.create();
-
-  for (const file of req.files) {
-    const pdf = await PDFDocument.load(file.buffer);
-    const pages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
-    pages.forEach((page) => mergedPdf.addPage(page));
-  }
-
-  const pdfBytes = await mergedPdf.save();
-
-  res.setHeader("Content-Type", "application/pdf");
-  res.send(pdfBytes);
-};
+mergePDF,
+splitPDF,
+compressPDF,
+pdfToWord,
+wordToPDF,
+pdfToJpg,
+jpgToPDF,
+rotatePDF,
+addPdfWatermark,
+removePdfPages
