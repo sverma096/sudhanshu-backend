091408295f0158a87ee75a7c86eff1b8e446c270
sverma096 <tiger543939@gmail.com>
2026-04-08 12:59:20 +0530

    Update excel.js

--- utils/excel.js
+++ utils/excel.js
@@ -1,12 +1,7 @@
-const XLSX = require("xlsx");
-
-exports.jsonToExcel = (data, res) => {
-  const ws = XLSX.utils.json_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, "Sheet");
-
-  const buffer = XLSX.write(wb, { type: "buffer", bookType: "xlsx" });
-
-  res.setHeader("Content-Type", "application/vnd.openxmlformats");
-  res.send(buffer);
-};
+excelToJson,
+jsonToExcel,
+csvToExcel,
+removeDuplicatesExcel,
+splitSheets,
+mergeSheets,
+analyzeExcel
